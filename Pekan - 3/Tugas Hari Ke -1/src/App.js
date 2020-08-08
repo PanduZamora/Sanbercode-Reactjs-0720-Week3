3c4b4aeb968311251c1a3b34b7beab303bfdc240
Pandu zmr <pandu.zamora091214@gmail.com>
2020-08-08 07:46:58 +0700

    Belajar Front End Pekan 3

---
+++ Pekan - 3/Tugas Hari Ke -1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Routes from './tugas15/Routes';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Routes />
+
+
+    </div>
+  );
+}
+
+export default App;
